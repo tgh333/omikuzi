0e88e802c8b4d9215b1b824cf53d8e2a001568bf
MYCOMPUTER\jakap <ys.training210629@gmail.com>
2022-05-08 22:59:43 +0900

    完成

--- js/script.js
+++ js/script.js
@@ -3,12 +3,17 @@
 const changeBtnTxt = () => {
     const $btnTxt = document.querySelector(".js-btn-txt");
     const $btn = document.querySelector(".js-btn");
+    const fortune = document.querySelector(".fortune");
+    let array = ["大吉","中吉","吉","小吉","末吉","凶","大凶"];
     $btn.addEventListener("click", () => {
         if($btnTxt.innerText == "PUSH!!") {
-            $btnTxt.innerText = "STOP!!";
+            $btnTxt.innerText = "RETURN";
+            let result = Math.floor(Math.random() * array.length);
+            fortune.textContent = array[result];
         } else {
             $btnTxt.innerText = "PUSH!!";
-        }    
+            fortune.textContent = "あなたの運勢は？";
+        }  
     });    
 }
 changeBtnTxt();
